05a2f7ccfad106177bfd19458f441b0a4c41f65e
brosilio <tarshadow5@gmail.com>
2025-01-28 23:20:14 -0500

    get port from env var

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express');
 const path = require('path');
 
@@ -5,7 +7,7 @@ const indexRouter = require("./routes/index");
 const postRouter = require("./routes/post");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.APP_PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
