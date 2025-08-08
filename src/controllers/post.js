59ed14b4bf6e142f344eec834258dc9fa01d22b7
brosilio <tarshadow5@gmail.com>
2025-08-07 21:55:42 -0400

    move post loading to post controller; cache posts; use chokidar to watch for file changes

---
+++ src/controllers/post.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+const postDirectory = process.env.POST_DIRECTORY;
+
+const fs = require("fs/promises");
+const path = require("path");
+const chokidar = require("chokidar");
+
+const postCache = new Map();
+chokidar
+	.watch(postDirectory, {
+		ignoreInitial: true,
+		ignorePermissionErrors: true,
+	})
+	.on("all", (event, filename) => {
+		const slug = path.basename(filename).slice(0, -3);
+		postCache.delete(slug);
+	});
+
+async function getPostFromFile(slug) {
+	const filePath = path.join(postDirectory, `${slug}.md`);
+
+	let raw = null;
+	try {
+		raw = await fs.readFile(filePath, "utf-8");
+	} catch (error) {
+		console.log(`Error loading post: ${error.message}`);
+		return false;
+	}
+
+	const match = raw.match(/\r?\n\r?\n/);
+	let metaBlock = "";
+	let content = raw;
+
+	if (match) {
+		metaBlock = raw.slice(0, match.index);
+		content = raw.slice(match.index + match[0].length);
+	} else {
+		return raw;
+	}
+
+	const metadata = {};
+	metaBlock.split(/\r?\n/).forEach((line) => {
+		const [key, ...rest] = line.split(":");
+		if (key && rest.length) {
+			let value = rest.join(":").trim();
+
+			if (key.trim().toLowerCase() === "tags") {
+				value = value
+					.split(",")
+					.map((tag) => tag.trim())
+					.filter(Boolean);
+			}
+
+			metadata[key.trim()] = value;
+		}
+	});
+
+	if (metadata.draft === "yes") {
+		return {
+			metadata,
+			content: "This post is marked as a draft and is not yet available",
+		};
+	}
+
+	return {
+		metadata,
+		content,
+	};
+}
+
+async function GetPostBySlug(slug) {
+	if (postCache.has(slug)) {
+		return postCache.get(slug);
+	}
+
+	let post = await getPostFromFile(slug);
+	postCache.set(slug, post);
+
+	return post;
+}
+
+module.exports = {
+	GetPostBySlug,
+};
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,28 +1,18 @@
 require("dotenv").config();
 const blogName = process.env.BLOG_NAME;
 
-const fs = require("fs/promises");
-const path = require("path");
 const MarkdownIt = require("markdown-it");
-
+const Post = require("../controllers/post");
 const express = require("express");
 const router = express.Router();
 const md = new MarkdownIt();
 
 router.get("/:slug", async (req, res) => {
 	const slug = req.params.slug;
-	const filePath = path.join(process.env.POST_DIRECTORY, `${slug}.md`);
 
-	try {
-		await fs.stat(filePath);
-	} catch (error) {
-		console.log(error);
-		return res.status(404).send("404 post not found");
-	}
-
-	const post = await parsePost(filePath);
+	const post = await Post.GetPostBySlug(slug);
 	if (post === false) {
-		return res.status(500).send("500 internal server error");
+		return res.status(404).send("404 post not found");
 	}
 
 	const html = md.render(post.content ? post.content : post);
@@ -34,55 +24,5 @@ router.get("/:slug", async (req, res) => {
 		content: html,
 	});
 });
-
-async function parsePost(filePath) {
-	let raw = null;
-	try {
-		raw = await fs.readFile(filePath, "utf-8");
-	} catch (error) {
-		console.log(`Error loading post: ${error}`);
-		return false;
-	}
-
-	const match = raw.match(/\r?\n\r?\n/);
-	let metaBlock = "";
-	let content = raw;
-
-	if (match) {
-		metaBlock = raw.slice(0, match.index);
-		content = raw.slice(match.index + match[0].length);
-	} else {
-		return raw;
-	}
-
-	const metadata = {};
-	metaBlock.split(/\r?\n/).forEach((line) => {
-		const [key, ...rest] = line.split(":");
-		if (key && rest.length) {
-			let value = rest.join(":").trim();
-
-			if (key.trim().toLowerCase() === "tags") {
-				value = value
-					.split(",")
-					.map((tag) => tag.trim())
-					.filter(Boolean);
-			}
-
-			metadata[key.trim()] = value;
-		}
-	});
-
-	if (metadata.draft === "yes") {
-		return {
-			metadata,
-			content: "This post is marked as a draft and is not yet available",
-		};
-	}
-
-	return {
-		metadata,
-		content,
-	};
-}
 
 module.exports = router;
