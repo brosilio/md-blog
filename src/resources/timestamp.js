167f504a1ef8b84db366d6be6009c4c7cb00c80c
brosilio <tarshadow5@gmail.com>
2025-08-08 22:00:54 -0400

    add time to modify date but be unspecific about it

--- src/resources/timestamp.js
+++ src/resources/timestamp.js
@@ -4,13 +4,39 @@ const timeFormat = {
 	weekday: "long",
 	month: "long",
 	day: "numeric",
+    year: "numeric"
 };
 
 function FormatFileTime(timestamp) {
+	if (!timestamp) return "(no date)";
+
 	timestamp = new Date(timestamp);
-	return timestamp
-		? timestamp.toLocaleDateString(TIME_LOCALE, timeFormat)
-		: "(no date)";
+
+	let baseDate = timestamp.toLocaleDateString(TIME_LOCALE, timeFormat);
+
+	const hour24 = timestamp.getHours();
+	const minutes = timestamp.getMinutes();
+
+	let hour = hour24 % 12 || 12;
+	const isPM = hour24 >= 12;
+	const suffix = isPM ? 'pm' : 'am';
+
+	const roundedMins = Math.round(minutes / 30) * 30;
+	const roundedHour = (roundedMins === 60) ? (hour % 12 || 12) + 1 : hour;
+	const roundedSuffix = (roundedMins === 60) ? (hour === 11 ? !isPM : isPM) ? 'pm' : 'am' : suffix;
+
+	const formatOptions = [
+		`like ${roundedHour}${roundedMins === 30 ? `:30` : ''}${roundedSuffix}`,
+		`around ${roundedHour}${roundedSuffix}`,
+		`${roundedHour}${roundedSuffix}-ish`,
+		`maybe ${roundedHour}${roundedSuffix}?`,
+		`somewhere near ${roundedHour}${roundedSuffix}`,
+		`uhhh ${roundedHour}${roundedSuffix} sorta`,
+	];
+
+	const fuzzyTime = formatOptions[Math.floor(Math.random() * formatOptions.length)];
+
+	return `${baseDate}, ${fuzzyTime}`;
 }
 
 module.exports = {
